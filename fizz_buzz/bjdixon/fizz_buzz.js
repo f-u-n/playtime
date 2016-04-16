24b31b0e9e4ba54b0883500a17c30f763ee402ac
Brian Dixon <bjdixon@gmail.com>
2016-04-16 05:50:54 -0400

    remove nested ternaries

--- fizz_buzz/bjdixon/fizz_buzz.js
+++ fizz_buzz/bjdixon/fizz_buzz.js
@@ -3,14 +3,17 @@
 const divisible = (denominator) => (numerator) => !(numerator % denominator)
 const divisibleBy3 = divisible(3)
 const divisibleBy5 = divisible(5)
+const maybePrint = (str) => (arg) => arg ? str : ''
+const fizz = maybePrint('fizz')
+const buzz = maybePrint('buzz')
 
 const fizzbuzz = (n) => {
-  console.log(divisibleBy3(n) ? divisibleBy5(n) ? 'fizzbuzz' : 'fizz' : divisibleBy5(n) ? 'buzz' : n)
-  if (n < times) {
-    fizzbuzz(n + 1)
+  if (n <= times) {
+    return (fizz(divisibleBy3(n)) + buzz(divisibleBy5(n)) || n) + fizzbuzz(n + 1)
   }
+  return ''
 }
 
-fizzbuzz(1)
+console.log(fizzbuzz(1))
 
 })(100)
