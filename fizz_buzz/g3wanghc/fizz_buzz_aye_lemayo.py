9c6c7883b6aa2214e21ee267a6b4c7aee39d113a
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-22 09:26:58 -0400

    PEP8 conventions and reverse Fizz/Buzz

--- fizz_buzz/g3wanghc/fizz_buzz_aye_lemayo.py
+++ fizz_buzz/g3wanghc/fizz_buzz_aye_lemayo.py
@@ -1,14 +1,15 @@
 def main():
     print ([fizz_buzz(n) for n in range(1, 100)])
 
+
 def fizz_buzz(n):
-	if (n % 15 == 0):
-		return 'FizzBuzz'
-	if (n % 5 == 0):
-		return 'Fizz'
-	if (n % 3 == 0):
-		return 'Buzz'
-	return n
-	
+    if (n % 15 == 0):
+        return 'FizzBuzz'
+    if (n % 3 == 0):
+        return 'Fizz'
+    if (n % 5 == 0):
+        return 'Buzz'
+    return n
+
 if __name__ == "__main__":
     main()
