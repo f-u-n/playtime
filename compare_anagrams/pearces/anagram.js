1d224c2c0e16be85dec03aac3dede67c5b094ca3
Stephen Pearce <stephen.r.pearce@gmail.com>
2016-04-26 23:15:23 -0400

    Moved some duplicated calls into a separate function

--- compare_anagrams/pearces/anagram.js
+++ compare_anagrams/pearces/anagram.js
@@ -4,6 +4,10 @@ const arrayize = (str) =>
   // convert string to array, remove spaces
   Array.prototype.slice.call(str.replace(/\s/g, ''));
 
+const flatten = (arr) =>
+  // sort and flatten an array to a string
+  arr.sort().join('');
+
 module.exports.compare = (str1, str2) =>
   // check that strings aren't the same but that the sorted/flattened strings are
-  str1 !== str2 && arrayize(str1).sort().join('') === arrayize(str2).sort().join('');
+  str1 !== str2 && flatten(arrayize(str1)) === flatten(arrayize(str2));
