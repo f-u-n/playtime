2b0ce98732e7ecae7aba155145905e4fde07dfcc
VishakhaSethi <VishakhaSethi@users.noreply.github.com>
2016-10-23 23:01:35 -0400

    Vishakha fizz buzz (#21)
    * Add files via upload
    
    * Update fizzBuzzQuiz.js
    
    fixed spacing issue
    
    * Rename fizz_buzz/fizzBuzzQuiz.js to fizz_buzz/vishS/fizzBuzzQuiz.js
    
    created a separate folder

---
+++ fizz_buzz/vishS/fizzBuzzQuiz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(max){
+	for(i=0; i<=max; i++){
+  		if(i % 3 == 0 && i % 5 == 0) {
+    			console.log("fizzbuzz");
+    		} else if(i % 3 == 0) {
+    			console.log("fizz");
+    		} else if(i % 5 == 0) {
+    			console.log("buzz");
+   		} else {
+    			console.log(i);
+    		}
+  	};
+}
+
+fizzBuzz(99);
