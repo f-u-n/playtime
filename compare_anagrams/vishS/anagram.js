8930d1388eae161662ceaf93f490a4619ea2e25e
VishakhaSethi <VishakhaSethi@users.noreply.github.com>
2016-10-28 00:21:30 -0400

    compare anagrams solution (#23)
    * Create anagram.js
    
    * Update anagram.js

---
+++ compare_anagrams/vishS/anagram.js
@@ -0,0 +1,12 @@
+function checkAnagram(string1, string2){
+    function stringToArray(str){
+        str = str.split("").sort().join().trim();   
+        return str;       
+    }    
+    if(string1.length == string2.length && stringToArray(string1) === stringToArray(string2)) {
+        return true;   
+    }
+    else {
+        return false;
+    }
+}
